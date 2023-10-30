46a09c03975bd0497602d3c9591745fbf1354ea1
André Lidre <andrelidre@mcno37hk0vg0.one.local>
2023-10-30 14:06:52 +0100

    a

--- thehive3.sh
+++ thehive3.sh
@@ -1,29 +1,20 @@
+apt-get install -y openjdk-8-jre-headless
+echo JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/environment
+export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+curl -fsSL https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
+echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+sudo apt update
+sudo apt install cassandra
+cqlsh localhost 9042
+cqlsh> UPDATE system.local SET cluster_name = 'thp' where key='local';
+nodetool flush
+service cassandra restart
+mkdir /opt/thp/thehive/index
+chown thehive:thehive -R /opt/thp/thehive/index
+mkdir -p /opt/thp/thehive/files
+chown -R thehive:thehive /opt/thp/thehive/files
+curl https://raw.githubusercontent.com/TheHive-Project/TheHive/master/PGP-PUBLIC-KEY | sudo apt-key add -
+echo 'deb https://deb.thehive-project.org release main' | sudo tee -a /etc/apt/sources.list.d/thehive-project.list
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install openjdk-11-jre-headless
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
-sudo apt install apt-transport-https
-sudo apt update && sudo apt install elasticsearch
-cd /opt
-wget https://download.thehive-project.org/thehive-latest.zip
-unzip thehive-latest.zip
-ln -s thehive-3.5.2-1 thehive
-sudo addgroup thehive
-sudo adduser --system thehive
-sudo cp /opt/thehive/package/thehive.service /usr/lib/systemd/system
-sudo chown -R thehive:thehive /opt/thehive
-sudo chgrp thehive /etc/thehive/application.conf
-sudo chmod 640 /etc/thehive/application.conf
-sudo systemctl enable thehive
-sudo service thehive start
-sudo mkdir /etc/thehive
-(cat << _EOF_
-# Secret key
-# ~~~~~
-# The secret key is used to secure cryptographics functions.
-# If you deploy your application to several instances be sure to use the same key!
-play.http.secret.key="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)"
-_EOF_
-) | sudo tee -a /etc/thehive/application.conf
-bin/thehive -Dconfig.file=/etc/thehive/application.conf
-
+sudo apt-get install thehive4
+service thehive start
